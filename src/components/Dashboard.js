1eb8f820a51068004e381ea9be89490d8f2d0be1
codeHysteria28 <graphicglobal67@gmail.com>
2021-01-12 09:37:22 +0000

    added header for dashboard + added some security auth checks when entering dashboard without login

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,40 +1,71 @@
-import React, {useState, useEffect} from "react";
+import React from "react";
 import axios from "axios";
+import Swal from 'sweetalert2';
+import Header from "./Header";
 
-const Dashboard = () => {
-    const [data, setData] = useState(null);
+class Dashboard extends React.Component  {
+    constructor(props) {
+        super(props);
 
-    const logout = () => {
+        this.state = {
+            data: null,
+            auth: false
+        }
+    }
+
+    logout = () => {
         axios({
             method: "post",
             url: "http://localhost:1998/logout",
             withCredentials: true,
         }).then((res) => {
-            window.location = '/';
+            window.location = '/login';
         });
     }
 
-    const getUser = () => {
+    getUser = () => {
         axios({
             method: "GET",
             withCredentials: true,
             url: "http://localhost:1998/user",
         }).then((res) => {
-            setData(res.data);
+            if(res.data !== ""){
+                this.setState({data: res.data});
+                this.setState({auth: true})
+            }else {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Error with authentication, please try again later.'
+                });
+
+                setTimeout(() => {
+                    this.logout();
+                }, 2000);
+            }
         });
     };
 
-    // useEffect(() => {
-    //     getUser();
-    // },[data]);
+    componentDidMount() {
+        this.getUser();
+    }
 
-    return (
-        <div>
-            {data ? <h1>Welcome Back {data.username}</h1> : null}
-            <button onClick={getUser}>Submit</button>
-            <button type="button" onClick={logout}>Logout</button>
-        </div>
-    );
+    render() {
+        return (
+            <div>
+                {this.state.auth
+                    ?
+                    <div>
+                        <Header/>
+                        <button onClick={this.getUser}>Submit</button>
+                        <button type="button" onClick={this.logout}>Logout</button>
+                        <p>{this.state.data ? this.state.data.username : null}</p>
+                        <p>{this.state.auth}</p>
+                    </div>
+                    : ""
+                }
+            </div>
+        );
+    }
 }
 
 export default Dashboard;
